9a5e57acdb559e04bde96f26caecb802e87349ef
rafavieira89 <raf.domingos@hotmail.com>
2024-01-31 22:36:56 -0300

    Codigo de estrutura de decisão

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("oi");
+let nomeHeroi = "Batman";
+let xpHeroi = 80050;
+
+if (xpHeroi < 1000) {
+  xpHeroi = "ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+  xpHeroi = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+  xpHeroi = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+  xpHeroi = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+  xpHeroi = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+  xpHeroi = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+  xpHeroi = "Imortal";
+} else if (xpHeroi >= 10001) {
+  xpHeroi = "Radiante";
+}
+
+console.log(`O herói de nome ${nomeHeroi} está no nível de ${xpHeroi}`);
