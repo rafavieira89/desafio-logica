23447202951996c8701d386607a9860f830d7f59
rafavieira89 <raf.domingos@hotmail.com>
2024-02-06 05:17:15 -0300

    Function FirstName

--- function.js
+++ function.js
@@ -1,5 +1,14 @@
-trocar("Pastilha de freio", "discos de freio");
+//trocar("Pastilha de freio", "discos de freio");
 
-function trocar(peca1, peca2) {
-  console.log(`Foi trocado a ${peca1} e  os ${peca2}`);
+//function trocar(peca1, peca2) {
+//console.log(`Foi trocado a ${peca1} e  os ${peca2}`);
+//}
+
+let userName = getFirstName("Rafael Domingos Veira");
+
+console.log(`Seja bem vindo ${userName}`);
+
+function getFirstName(name) {
+  let firstName = name.split(" ")[0];
+  return firstName;
 }
